f5f138a97d9128c797a19833a8f76be7e8d0ab3a
Hasenko <imaddine25113003@gmail.com>
2024-12-15 22:38:12 +0100

    removing useless import

--- HELBArmy/Main.java
+++ HELBArmy/Main.java
@@ -1,17 +1,5 @@
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class Main extends Application {
 
