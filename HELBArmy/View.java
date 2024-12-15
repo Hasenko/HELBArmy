c585661f6e60ae8ad7bf344befa327d50b834653
Hasenko <imaddine25113003@gmail.com>
2024-12-15 22:36:19 +0100

    refactor: changing responsability, add: possibility to view Pikemen range

--- HELBArmy/View.java
+++ HELBArmy/View.java
@@ -1,16 +1,42 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
 public class View {
-    private final HELBArmy CONTROLER;
+    private final HELBArmy controller;
     public final int WIDTH = 800;
     public final int HEIGHT = WIDTH;
+    private GraphicsContext gc;
+    private Scene scene;
 
-    public View(HELBArmy controler) {
-        this.CONTROLER = controler;
+    public View(HELBArmy controller, Stage primaryStage) {
+        this.controller = controller;
+
+        primaryStage.setTitle("HELBArmy");
+        Group root = new Group();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        this.scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        this.gc = canvas.getGraphicsContext2D();
+
+    }
+
+    public Scene getScene()
+    {
+        return this.scene;
+    }
+
+    public GraphicsContext getGraphicsContext()
+    {
+        return this.gc;
     }
 
     /*
@@ -18,14 +44,14 @@ public class View {
     */
     public void drawBackground(GraphicsContext gc)
     {
-        for (int i = 0; i < CONTROLER.ROWS; i++) {
-            for (int j = 0; j < CONTROLER.COLUMNS; j++) {
+        for (int i = 0; i < HELBArmy.ROWS; i++) {
+            for (int j = 0; j < HELBArmy.COLUMNS; j++) {
                 if ((i + j) % 2 == 0) {
                     gc.setFill(Color.web("AAD751"));
                 } else {
                     gc.setFill(Color.web("A2D149"));
                 }
-                gc.fillRect(i * CONTROLER.SQUARE_SIZE, j * CONTROLER.SQUARE_SIZE, CONTROLER.SQUARE_SIZE, CONTROLER.SQUARE_SIZE);
+                gc.fillRect(i * controller.SQUARE_SIZE, j * controller.SQUARE_SIZE, controller.SQUARE_SIZE, controller.SQUARE_SIZE);
             }
         }
     }
@@ -36,7 +62,7 @@ public class View {
     public void drawEntity(GraphicsContext gc, ArrayList<Entity> entityList)
     {
         for (Entity entity : entityList) {
-            gc.drawImage(new Image(entity.getImagePath()), entity.position.x * CONTROLER.SQUARE_SIZE, entity.position.y * CONTROLER.SQUARE_SIZE, CONTROLER.SQUARE_SIZE * entity.getWidth(), CONTROLER.SQUARE_SIZE * entity.getWidth());
+            gc.drawImage(new Image(entity.getImagePath()), entity.position.x * controller.SQUARE_SIZE, entity.position.y * controller.SQUARE_SIZE, controller.SQUARE_SIZE * entity.getWidth(), controller.SQUARE_SIZE * entity.getWidth());
             
             // if (entity instanceof Collector)
             // {
@@ -58,6 +84,16 @@ public class View {
                 }
             }
             */
+
+            // SHOW PIKEMEN ACCESSIBLE POSITION
+            if (entity instanceof Pikemen)
+            {
+                for (Position position : entity.getAccessibleAdjacentPositions())
+                {
+                    gc.setFill(Color.web("FF00FF"));
+                    gc.fillRoundRect(position.x * controller.SQUARE_SIZE, position.y * controller.SQUARE_SIZE, controller.SQUARE_SIZE - 1, controller.SQUARE_SIZE - 1, 35, 35);
+                }
+            }
 
         }
     }
