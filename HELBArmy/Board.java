d2c883f62e8639e72ecf4b690c13a2c04cee9497
Hasenko <imaddine25113003@gmail.com>
2024-12-28 15:11:04 +0100

    refactor: changing comment

--- HELBArmy/Board.java
+++ HELBArmy/Board.java
@@ -1,17 +1,12 @@
 public class Board {
     
-    // ECAMPUS
+    // know if a position is in board
     public static boolean isPositionInBoard(Position pos){
         return(pos.x >= 0 && pos.y >= 0 && pos.x < HELBArmy.COLUMNS && pos.y < HELBArmy.ROWS); 
     }
 
-    /*
-        get distance between two position
-    */
-    public static double getDistance(Position pos1, Position pos2)
-    {
-        // √((x_2-x_1)²+(y_2-y_1)²)
-
+    // get distance between two position | √((x_2-x_1)²+(y_2-y_1)²)
+    public static double getDistance(Position pos1, Position pos2) {
         return Math.sqrt((Math.pow(pos1.x - pos2.x, 2) + (Math.pow(pos1.y - pos2.y, 2))));
     }
 }
